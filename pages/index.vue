12a60dd4edc0ab0359bdcf35cafbfc33890365a5
Xyrai <Xyraidev@gmail.com>
2022-09-08 21:46:05 +0200

    Modified homepage content

--- pages/index.vue
+++ pages/index.vue
@@ -5,13 +5,9 @@
         <div class="row">
           <div class="col-12">
             <h1 class="text-3xl font-semibold text-center">
-              {{ data?.TotaalAantalObjecten }} {{ data?.Metadata.Titel }}
+              Uitgelichte koopwoningen
             </h1>
-            <p class="text-center my-3">
-              {{ data?.Metadata.Omschrijving }}
-            </p>
             <hr class="my-3" />
-            <!-- <UiButton>Test</UiButton> -->
           </div>
         </div>
       </div>
